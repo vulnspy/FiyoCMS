8a915166c204725b71e66aa56f879d8564e12844
Ambulong <zeng.ambulong@gmail.com>
2019-08-06 03:20:06 +0800

    use image

--- html/web/plugins/plg_ckeditor/plugins/saveajax/fileicon/lang/en.js
+++
@@ -1,7 +0,0 @@
-﻿CKEDITOR.plugins.setLang("fileicon","en",{
-  fileicon:
-    {
-	 toolbar:'Insert File Icon',
-	 title:'Insert File Icon'
-	}
-});
--- html/web/plugins/plg_ckeditor/plugins/saveajax/fileicon/lang/zh-cn.js
+++
@@ -1,7 +0,0 @@
-﻿CKEDITOR.plugins.setLang("fileicon","zh-cn",{
-  fileicon:
-    {
-	 toolbar:'插入图标',
-	 title:'插入文件图标'
-	}
-});
